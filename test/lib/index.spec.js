a2ab931d2ef385491f55e7473a0a84a0dcdb5136
sverweij <sverweij@users.noreply.github.com>
2020-03-15 19:58:29 +0100

    bugfix(*): add new rule to index.js
    and add a test so we cannot forget that

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -1,8 +1,20 @@
-const { rules } = require("../../lib/");
+const fs = require("fs");
+const path = require("path");
 const expect = require("chai").expect;
+const { rules } = require("../../lib/");
 
 describe("plugin index", () => {
   it("should have a parameter-pattern rule", () => {
     expect(rules).to.haveOwnProperty("parameter-pattern");
+  });
+
+  it("should have an entry in index.js for each rule in the rules folder", () => {
+    expect(
+      fs
+        .readdirSync(path.join("lib", "rules"))
+        .filter(pFileName => !pFileName.match(/utl/))
+        .map(pFileName => pFileName.split(".").shift())
+        .sort()
+    ).to.deep.equal(Object.keys(rules).sort());
   });
 });
