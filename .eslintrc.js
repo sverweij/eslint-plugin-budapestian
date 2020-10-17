1f6f5d06b27d2d5b1f3c2bb4438517e517d52259
Sander Verweij <sverweij@users.noreply.github.com>
2020-10-17 20:45:39 +0200

    feature: adds a 'recommended' preset (#14)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,7 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  plugins: ["unicorn", "local-budapestian"],
-  extends: ["eslint:recommended", "prettier"],
-  rules: {
-    "local-budapestian/parameter-pattern": "error",
-    "local-budapestian/local-variable-pattern": [
-      "error",
-      { exceptions: ["i", "j", "k"] },
-    ],
-    "local-budapestian/global-variable-pattern": "error",
-    "local-budapestian/global-constant-pattern": "error",
-  },
+  extends: ["eslint:recommended", "prettier", "plugin:budapestian/recommended"],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
