3058a16a132107c8b43c1cba40c1a97568288aaa
sverweij <sverweij@users.noreply.github.com>
2023-08-10 21:32:54 +0200

    refactor(test): replaces chai with node's native assertion library

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -1,22 +1,24 @@
+/* eslint-disable no-prototype-builtins */
 const fs = require("fs");
 const path = require("path");
-const expect = require("chai").expect;
 const { rules } = require("../../lib/");
+const { deepEqual, equal } = require("node:assert/strict");
 
 describe("plugin index", () => {
   it("should have a parameter-pattern rule", () => {
-    expect(rules).to.haveOwnProperty("parameter-pattern");
+    equal(rules.hasOwnProperty("parameter-pattern"), true);
   });
 
   it("should have an entry in index.js for each rule in the rules folder", () => {
-    expect(
+    deepEqual(
       fs
         .readdirSync(path.join("lib", "rules"))
         .filter((pFileName) => pFileName.match(/-rule\.js$/))
         .map(
           (pFileName) => pFileName.match(/^(?<name>\S*)-rule\.js$/).groups.name
         )
-        .sort()
-    ).to.deep.equal(Object.keys(rules).sort());
+        .sort(),
+      Object.keys(rules).sort()
+    );
   });
 });
--- test/lib/rules/ast-utl.spec.js
+++ test/lib/rules/ast-utl.spec.js
@@ -1,35 +1,38 @@
 const astUtl = require("../../../lib/rules/ast-utl.js");
-const expect = require("chai").expect;
+const { equal } = require("node:assert/strict");
 
 describe("ast-utl - getVariableDeclaratorName edge cases", () => {
   it("returns 'OK_ANYWAY' if the variable declarator is an empty object", () => {
-    expect(astUtl.getVariableDeclaratorName({})).to.equal("OK_ANYWAY");
+    equal(astUtl.getVariableDeclaratorName({}), "OK_ANYWAY");
   });
   it("returns 'OK_ANYWAY' if the variable declarator is an object with an id object without a name", () => {
-    expect(astUtl.getVariableDeclaratorName({ id: {} })).to.equal("OK_ANYWAY");
+    equal(astUtl.getVariableDeclaratorName({ id: {} }), "OK_ANYWAY");
   });
   it("returns the name if the variable declarator is an object with an id object that has a name", () => {
-    expect(
-      astUtl.getVariableDeclaratorName({ id: { name: "A_NAME" } })
-    ).to.equal("A_NAME");
+    equal(
+      astUtl.getVariableDeclaratorName({ id: { name: "A_NAME" } }),
+      "A_NAME"
+    );
   });
 });
 
 describe("ast-utl - getParameterDeclaratorName edge cases", () => {
   it("returns 'pOK' if the variable declarator is an empty object", () => {
-    expect(astUtl.getParameterDeclaratorName({})).to.equal("pOK");
+    equal(astUtl.getParameterDeclaratorName({}), "pOK");
   });
   it("returns 'pOK' if the variable declarator is an object with an left object without a name", () => {
-    expect(astUtl.getParameterDeclaratorName({ left: {} })).to.equal("pOK");
+    equal(astUtl.getParameterDeclaratorName({ left: {} }), "pOK");
   });
   it("returns the name if the variable declarator is an object with an id object that has a name", () => {
-    expect(astUtl.getParameterDeclaratorName({ name: "pParameter" })).to.equal(
+    equal(
+      astUtl.getParameterDeclaratorName({ name: "pParameter" }),
       "pParameter"
     );
   });
   it("returns the name if the variable declarator is an object with an id object that has a name", () => {
-    expect(
-      astUtl.getParameterDeclaratorName({ left: { name: "pParameter" } })
-    ).to.equal("pParameter");
+    equal(
+      astUtl.getParameterDeclaratorName({ left: { name: "pParameter" } }),
+      "pParameter"
+    );
   });
 });
